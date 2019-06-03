2c829ca4c88eefcaae86131cfc15f3d8ab51b1a0
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-06-03 14:47:54 +0100

    written test to be able to add new transaction to list

--- spec/statememt_spec.rb
+++ spec/statememt_spec.rb
@@ -7,4 +7,13 @@ describe Statement do
       expect(statement.show).to eq []
     end
   end
+
+  describe 'list of transactions' do
+    it 'can add new transaction' do
+      statement = Statement.new
+      transaction = Transaction.new(20, nil, 80)
+      statement.show << transaction
+      expect(statement.show).to eq [transaction]
+    end
+  end
 end
