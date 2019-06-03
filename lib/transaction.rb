ccbe3ba2cd0086b6c6da78f1c86ffad80f1eeb8e
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-06-03 14:00:18 +0100

    installed timecop bundle and wrote feature test for making withdrawal

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,11 +1,12 @@
 class Transaction
 
-  attr_reader :credit, :debit, :balance
+  attr_reader :credit, :debit, :balance, :date
 
   def initialize(credit, debit, balance)
     @credit = credit
     @balance = balance
     @debit = debit
+    @date = Time.now
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -9,5 +9,12 @@ describe Transaction do
       expect(transaction.debit).to eq nil
       expect(transaction.balance).to eq 80
     end
+
+    it 'make a withdrawal' do
+      transaction = Transaction.new(nil, 20, 80)
+      expect(transaction.credit).to eq nil
+      expect(transaction.debit).to eq 20
+      expect(transaction.balance).to eq 80
+    end
   end
 end
